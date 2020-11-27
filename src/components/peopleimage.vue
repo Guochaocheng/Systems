b7ad96e93173c9ce9c1b8e30af4878c361f1fad9
lmislm <wszdlm@gmail.com>
2020-11-27 15:48:17 +0800

    feat: first data

--- src/components/peopleimage.vue
+++ src/components/peopleimage.vue
@@ -7,9 +7,24 @@
                 :src="images[i]">
             </el-image>
         </div>
+        <div class="people-form">
+          <div class="people-item" v-for="(item, i) in peopleInfos" :key="item + i">
+            <div class="base">
+              <div class="name">{{item.name}}</div>
+              <div class="age">{{item.age}}</div>
+            </div>
+            <div class="pic-wrapper" v-for="(el, index) in item.images" :key="el + index">
+              <el-image
+                  style="width: 100px; height: 100px"
+                  :src="el[i]">
+              </el-image>
+            </div>
+          </div>
+        </div>
     </div>
 </template>
 <script>
+import peopleJson from '@/mock/people'
 export default {
   name: 'peopleimage',
   data () {
@@ -17,7 +32,12 @@ export default {
       images: ['../assets/Wangxu.jpg', '../assets/logo.png'],
       names: ['wangxu', 'logo'],
       directions: ['A', 'B'],
-      mails: ['a@163', 'b@163']
+      mails: ['a@163', 'b@163'],
+      peopleInfos: peopleJson
+    }
+  },
+  methods: {
+    getData () {
     }
   }
 }
---
+++ src/mock/people.js
@@ -0,0 +1,9 @@
+
+const peoples = [
+  {
+    name: 'gcc',
+    age: '17',
+    iamges: ['../assets/Wangxu.jpg', '../assets/logo.png']
+  }
+]
+export default peoples
